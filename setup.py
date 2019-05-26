fc648bcef0f73d382c9cf93612d2d78bc6b29b0f
Ivan Arar <ivan.arar@gmail.com>
2019-05-26 16:41:17 +0200

    py version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5, 3.6, 3.7',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='marine traffic, api, cruise, distance, port, vessel, track, fleet',
 
