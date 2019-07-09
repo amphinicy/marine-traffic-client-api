0cf4eb27f22bd9d965dda821704a923992d60acc
Ivan Arar <ivan.arar@amphinicy.com>
2019-07-09 11:20:20 +0200

    travis py 3.8 removed

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        #'Programming Language :: Python :: 3.8',
     ],
     keywords='marine traffic, api, cruise, distance, port, vessel, track, fleet',
 
